c33541b7db97ba2b1924ef2386f7bf751e3e5115
jaeyoung kim <jykim@whatap.io>
2023-05-16 14:38:15 +0900

    upated log trace(middleware, dependency)

--- fastapi_0.95/server.py
+++ fastapi_0.95/server.py
@@ -1,26 +1,18 @@
 from loguru import logger
 from datetime import datetime, timedelta
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, Request
 from starlette.responses import HTMLResponse
+from starlette.middleware.errors import ServerErrorMiddleware
+from fastapi.exception_handlers import http_exception_handler
 import uvicorn
 import requests
 
-try:
-
-    import whatap.trace.mod.logging as whatap_logging
-    if whatap_logging.loguru_injection_processed:
-        # whatap 모니터링 사용하는 경우
-        logger.add('whatap_sink.log', level='DEBUG', format="<level>{level: <8}</level> <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>  -- {{ \"@txid\" : \"{txid}\" }} -- <cyan>{name}</cyan>:<cyan>{function}</cyan> - <level>{message}</level>")
-    else:
-        # whatap 모니터링 사용, loguru injection (x)
-        logger.add('app.log', level='DEBUG', format="<level>{level: <8}</level> <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> <cyan>{name}</cyan>:<cyan>{function}</cyan> - <level>{message}</level>")
-except:
-    # whatap 모니터링 사용하지 않는 경우
-    logger.add('app.log', level='DEBUG', format="<level>{level: <8}</level> <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> <cyan>{name}</cyan>:<cyan>{function}</cyan> - <level>{message}</level>")
-
 app = FastAPI()
+
+app.add_middleware(middleware_class=ServerErrorMiddleware, handler=http_exception_handler)
+
 @app.get("/health_check")
-async def example():
+async def health_check():
     logger.info("whatap_loguru_1.3.4:/health_check")
     start_time = datetime.now()
     while True:
@@ -39,15 +31,39 @@ async def example():
     res = requests.get(url="https://www.naver.com")
     return HTMLResponse(status_code=res.status_code)
 
+async def log_test(request:Request):
+    logger.info(f"dependency")
+    return request.url
+
 @app.get("/sync_example", response_class=HTMLResponse)
-async def sync_example():
-    logger.info("whatap_loguru_1.3.4:/sync_example")
+def sync_example(test: str = Depends(log_test)):
+    logger.info(f"whatap_loguru_1.3.4:/sync_example {test}")
     start_time = datetime.now()
     while True:
         if datetime.now() - start_time > timedelta(seconds=1):
             break
     res = requests.get(url="https://www.naver.com")
     return HTMLResponse(status_code=res.status_code)
+
+@app.get("/async_example", response_class=HTMLResponse)
+async def async_example(test: str = Depends(log_test)):
+    logger.info(f"whatap_loguru_1.3.4:/async_example {test}")
+    start_time = datetime.now()
+    while True:
+        if datetime.now() - start_time > timedelta(seconds=1):
+            break
+    res = requests.get(url="https://www.naver.com")
+    return HTMLResponse(status_code=res.status_code)
+
+@app.get("/async_exception_example", response_class=HTMLResponse)
+async def async_exception_example(test: str = Depends(log_test)):
+    logger.info(f"whatap_loguru_1.3.4:/async_exception_example {test}")
+    start_time = datetime.now()
+    while True:
+        if datetime.now() - start_time > timedelta(seconds=1):
+            break
+    requests.get(url="https://www.naver.com")
+    raise HTTPException(status_code=400)
 
 if __name__ == "__main__":
     uvicorn.run(app="server:app", host="0.0.0.0", port=9000, reload=True)
